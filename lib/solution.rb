30f132af827b0ffe133e4b9361fc22fc98ddeb05
bosmanpa <bsmn.ptrck@gmail.com>
2019-10-22 09:48:56 -0500

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,15 @@
 def clock_angle(time)
-    # code your solution here
-end
+    time_array = time.split(":")
+    hour_deg = time_array[0].to_i * 30
+    min_deg = time_array[1].to_i * 0.5
+    min_rev = time_array[1].to_i * 6
+    degs = (hour_deg + min_deg) - min_rev
+    
+    if degs >= 360
+      degs = degs - 360.0
+    end
+    if degs > 180
+      degs = degs - 180
+    end
+    degs
+  end
