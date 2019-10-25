1a93155d5e8a56c1134620cc07acd25eda07bfb9
Daniel Ionita <danielionita@Daniels-MacBook-Pro.local>
2019-10-25 10:22:29 -0500

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,17 @@
+
 def clock_angle(time)
     # code your solution here
+    hour_minute = time.split(":")
+    
+    hour_degree = hour_minute[0].to_i * 30
+    minute_degree = hour_minute[1].to_i * 0.5
+    min_rev = hour_minute[1].to_i * 6
+
+    angle = (hour_degree + minute_degree) - min_rev
+    if angle >= 360
+        angle = angle - 360.0
+    elsif angle > 180
+        angle = angle - 180
+    end
+    angle
 end
